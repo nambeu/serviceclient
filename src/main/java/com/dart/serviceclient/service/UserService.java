9f2ff82e33a99717436a8143466b939ee0a50490
donald <dimitrieloundou@yahoo.fr>
2014-04-30 16:52:15 +0100

    first version

--- src/main/java/com/dart/serviceclient/service/UserService.java
+++ src/main/java/com/dart/serviceclient/service/UserService.java
@@ -4,4 +4,7 @@ import org.springframework.roo.addon.layers.service.RooService;
 
 @RooService(domainTypes = { com.dart.serviceclient.domain.UserAccount.class })
 public interface UserService {
+	public boolean isAnEmail(String email);
+	public String generatePassword();
+	public boolean sendPassword(String email,String password);
 }
--- src/main/java/com/dart/serviceclient/service/UserServiceImpl.java
+++ src/main/java/com/dart/serviceclient/service/UserServiceImpl.java
@@ -1,5 +1,30 @@
 package com.dart.serviceclient.service;
 
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Service;
+
+import com.dart.serviceclient.domain.UserAccount;
+@Service
 public class UserServiceImpl implements UserService {
+	@Override
+	public boolean isAnEmail(String email){
+		boolean result=false;
+		ArrayList<UserAccount> accounts=(ArrayList<UserAccount>)this.findAllUserAccounts();
+		for(int i=0;i<accounts.size();i++){
+			if(accounts.get(i).getEmail().equals(email)){
+				result=true;
+				break;
+			}
+		}
+		return result;
+	}
+	@Override
+	public String generatePassword(){
+		return "this is the password";
+	}
+	@Override
+	public boolean sendPassword(String email,String password){
+		return true;
+	}
 }
