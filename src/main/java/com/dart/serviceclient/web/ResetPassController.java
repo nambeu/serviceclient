d75c22e82b811bbe875dc4f6138731d96a93e261
dassiorleando <dassi_orleando@yahoo.fr>
2014-05-08 11:29:11 +0100

    has the pass word of user who want to reset her it

--- src/main/java/com/dart/serviceclient/web/ResetPassController.java
+++ src/main/java/com/dart/serviceclient/web/ResetPassController.java
@@ -7,18 +7,17 @@ import java.util.ArrayList;
 
 import javax.validation.Valid;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.dart.serviceclient.domain.ResetPasswordBean;
 import com.dart.serviceclient.domain.UserAccount;
-import com.dart.serviceclient.service.UserServiceImpl;
+import com.dart.serviceclient.service.UserService;
 
 /**
  * @author dassi
@@ -30,7 +29,7 @@ import com.dart.serviceclient.service.UserServiceImpl;
 public class ResetPassController {
 
 	@Autowired
-	UserServiceImpl userService;
+	UserService userService;
 
 	@RequestMapping(value = "/resetpasswd1")
 	public String passWd(Model uiModel) {
@@ -46,89 +45,44 @@ public class ResetPassController {
 		if (bindingResult.hasErrors()) {
 			return "changePass";
 		}
-
-		// //////////:::::test des binding errores
-		// personalisés:::://////////////
-
-		// if(resetPasswordBean.getUserName().isEmpty()){
-		//
-		// bindingResult = bindingResult + "dassi";
-		// }
-		//
-
-		// ///////////////////////////
-
-		// List<UserAccount> userAccounts =
-		// useraccount.findByUserNameAndPassword(
-		// resetPasswordBean.getUserName(),
-		// resetPasswordBean.getPassword());
-		// System.out.println("Results : " + userAccounts.toString());
+		String pass = DigestUtils.sha256Hex(resetPasswordBean.getPassword());
 
 		boolean testPassWd = userService.passWordDiff(
 				resetPasswordBean.getPasswordOne(),
 				resetPasswordBean.getPasswordTwo());
 
-		
-			ArrayList<UserAccount> myUsers;
-			myUsers = (ArrayList<UserAccount>) userService
-					.findAllUserAccounts();
+		ArrayList<UserAccount> myUsers;
+		myUsers = (ArrayList<UserAccount>) userService.findAllUserAccounts();
 
-			for (UserAccount user : myUsers) {
+		for (UserAccount user : myUsers) {
 
-				String bestOrBadSet;
-				if (user.getUserName().equals(resetPasswordBean.getUserName())
-						&& user.getPassword().equals(
-								resetPasswordBean.getPassword())) {
+			String bestOrBadSet;
 
-					user.setPassword(resetPasswordBean.getPasswordOne());
-					user.setConfirmPassword(resetPasswordBean.getPasswordOne());
+			if (user.getUserName().equals(resetPasswordBean.getUserName())
+					&& user.getPassword().equals(pass)) {
+				String newPass = DigestUtils.sha256Hex(resetPasswordBean
+						.getPasswordOne());
 
-					userService.updateUserAccount(user);
+				user.setPassword(newPass);
+				user.setConfirmPassword(newPass);
 
-					bestOrBadSet = " You have set your pass word successffully";
+				userService.updateUserAccount(user);
 
-				} else
-					bestOrBadSet = " You are not a user of this website, so this service is not available for you !!!!!!";
-					uiModel.addAttribute("bestSet", bestOrBadSet);
-			}
+				bestOrBadSet = " You have set your pass word successffully";
+
+			} else
+				bestOrBadSet = " You are not a user of this website, so this service is not available for you !!!!!!";
+			uiModel.addAttribute("bestSet", bestOrBadSet);
+		}
 
 		if (testPassWd) {
-		
-		
-			
+
 		} else {
 			String bestTwoPass = " the two new pass word are not same, please refill then form correctly !!!!!! ";
 			uiModel.addAttribute("bestTwoPass", bestTwoPass);
-
 		}
 
 		return "changePass";
-
-	}
-
-	@RequestMapping(value = "/newUser")
-	public String NewUser(Model uiModel) {
-		System.out.println(" un nouvel utilisateurs dans la base de données :");
-
-		return "resetPassController/newUser";
-	}
-
-	@RequestMapping(value = "/ajaxTestOne")
-	public String testAjaxOneShow(Model uiModel) {
-
-		return "ajaxTest";
-	}
-
-	@RequestMapping(value = "/ajaxTest", method = RequestMethod.POST)
-	public @ResponseBody String testAjaxOne(@RequestParam("user") String user,
-			@RequestParam("pass") String pass, Model uiModel) {
-
-		System.out.println(user);
-		System.out.println(pass);
-
-		String str = user + pass;
-
-		return str;
 	}
 
 }
