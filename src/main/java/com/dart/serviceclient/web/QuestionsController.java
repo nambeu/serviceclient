6706a087483657251d94410e817c2dbbfdca7c68
dassiorleando <dassi_orleando@yahoo.fr>
2014-05-26 20:01:08 +0100

    new add to the branch newTest

--- src/main/java/com/dart/serviceclient/web/QuestionsController.java
+++ src/main/java/com/dart/serviceclient/web/QuestionsController.java
@@ -1,11 +1,38 @@
 package com.dart.serviceclient.web;
-import com.dart.serviceclient.domain.QuestionToEntreprise;
+
+import javax.validation.Valid;
+
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dart.serviceclient.domain.QuestionToEntreprise;
 
 @RequestMapping("/questiontoentreprises")
 @Controller
 @RooWebScaffold(path = "questiontoentreprises", formBackingObject = QuestionToEntreprise.class)
 public class QuestionsController {
+
+	@RequestMapping(value = "/createQuestion")
+	public String showFormCreate(Model uiModel) {
+
+		QuestionToEntreprise questionToEntreprise = new QuestionToEntreprise();
+		uiModel.addAttribute("questionToEntreprise", questionToEntreprise);
+		return "questionsUsers";
+	}
+
+	@RequestMapping(value = "/postQuestion", method = RequestMethod.POST)
+	public String postQuestion(Model uiModel,
+			@Valid QuestionToEntreprise questionToEntreprise, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()){
+			return "questionsUsers";
+		}
+
+		return "listQuestionsUser";
+	}
+
 }
